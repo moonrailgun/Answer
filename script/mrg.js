157df3637419dc1ecc5f20a2b18803306acfee3d
moonrailgun <moonrailgun@gmail.com>
2015-11-01 11:37:01 +0800

    资料库通用显示列表
    ADD 资料库通用显示列表
    UPDATE OpenDocumentFile方法归档

--- script/mrg.js
+++ script/mrg.js
@@ -14,3 +14,61 @@ function GetRandomValColor(){
 	var randomColorCls = ['default', 'primary', 'success', 'info', 'warning', 'danger', 'blue', 'pink', 'dark'];
     return GetRandomValFromArray(randomColorCls);
 }
+
+//打开文档
+function OpenDocumentFile(url, name) {
+    var temp = name.split('.');
+    var type = temp[temp.length -1];
+
+    api.download({
+        url: url,
+        savePath: 'cache://' + name,
+        report: true,
+        cache: true,
+        allowResume: false
+    }, function (ret, err) {
+        if (ret) {
+            if (ret.state == 0) {
+                api.showProgress({
+                    style: 'default',
+                    animationType: 'fade',
+                    title: '正在打开...',
+                    text: ret.percent + '%'
+                });
+            }
+            else if (ret.state == 1) {
+                api.hideProgress();
+                if(type == 'doc' || type == 'docx' || type == 'xls' || type == 'xlsx'||type=='txt'){
+                    var docReader = api.require('docReader');
+                    docReader.open({
+                        path: 'cache://' + name
+                    });
+                }
+                else if(type == 'pdf'){
+                    var pdfReader = api.require('pdfReader');
+                    pdfReader.open({
+                        path:'cache://' + name
+                    });
+                }
+                else{
+                    api.toast({
+                        msg: '打开失败',
+                        duration: 2000,
+                        location: 'bottom'
+                    });
+                }
+            }
+            else if (ret.state == 2) {
+                api.hideProgress();
+                api.toast({
+                    msg: '打开失败,无法打开后缀为'+ type + '的文件',
+                    duration: 2000,
+                    location: 'bottom'
+                });
+            }
+        } else {
+            api.hideProgress();
+            api.alert({msg: err.msg});
+        }
+    });
+}
