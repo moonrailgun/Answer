a10c4cfdeb51a55a0f771828c25e7773b4c3eab9
moonrailgun <moonrailgun@gmail.com>
2015-11-01 15:12:55 +0800

    资料列表与下载管理
    ADD 资料列表UI
    ADD 添加下载

--- script/mrg.js
+++ script/mrg.js
@@ -9,16 +9,16 @@ function GetRandomValFromArray(arr) {
     var index = Math.floor(random * length);//[0,length-1]
     return arr[index];
 }
-function GetRandomValColor(){
-	//随机返回数组中的一个值
-	var randomColorCls = ['default', 'primary', 'success', 'info', 'warning', 'danger', 'blue', 'pink', 'dark'];
+function GetRandomValColor() {
+    //随机返回数组中的一个值
+    var randomColorCls = ['default', 'primary', 'success', 'info', 'warning', 'danger', 'blue', 'pink', 'dark'];
     return GetRandomValFromArray(randomColorCls);
 }
 
-//打开文档
+//下载到缓存并打开文档
 function OpenDocumentFile(url, name) {
     var temp = name.split('.');
-    var type = temp[temp.length -1];
+    var type = temp[temp.length - 1];
 
     api.download({
         url: url,
@@ -38,19 +38,19 @@ function OpenDocumentFile(url, name) {
             }
             else if (ret.state == 1) {
                 api.hideProgress();
-                if(type == 'doc' || type == 'docx' || type == 'xls' || type == 'xlsx'||type=='txt'){
+                if (type == 'doc' || type == 'docx' || type == 'xls' || type == 'xlsx' || type == 'txt') {
                     var docReader = api.require('docReader');
                     docReader.open({
                         path: 'cache://' + name
                     });
                 }
-                else if(type == 'pdf'){
+                else if (type == 'pdf') {
                     var pdfReader = api.require('pdfReader');
                     pdfReader.open({
-                        path:'cache://' + name
+                        path: 'cache://' + name
                     });
                 }
-                else{
+                else {
                     api.toast({
                         msg: '打开失败',
                         duration: 2000,
@@ -61,7 +61,7 @@ function OpenDocumentFile(url, name) {
             else if (ret.state == 2) {
                 api.hideProgress();
                 api.toast({
-                    msg: '打开失败,无法打开后缀为'+ type + '的文件',
+                    msg: '打开失败,无法打开后缀为' + type + '的文件',
                     duration: 2000,
                     location: 'bottom'
                 });
@@ -69,6 +69,49 @@ function OpenDocumentFile(url, name) {
         } else {
             api.hideProgress();
             api.alert({msg: err.msg});
+        }
+    });
+}
+//添加到下载队列
+function EnqueueFile(url, name) {
+    var use3g = false;
+    api.getPrefs({
+        key: '3g'
+    }, function (ret, err) {
+        use3g = ret.value;
+    });
+
+    var networkType = 'wifi';
+    if (use3g) {
+        networkType = 'all';
+    }
+
+    var manager = api.require('downloadManager');
+    manager.openManagerView({
+        title: '下载管理'
+    },function(ret){
+        var id = ret.id;
+        var mimeType = ret.mimeType;
+        var savePath = ret.savePath;
+        manager.openDownloadedFile({
+            id: id
+        },function(ret,err){
+            if (ret.status) {
+
+            } else {
+                var msg = ret.msg;
+            }
+        });
+    });
+    manager.enqueue({
+        url: url,
+        savePath: 'fs://Document/' + name,
+        cache: true,
+        title: name,
+        networkTypes: networkType
+    }, function (ret, err) {
+        if (ret.id) {
+            api.toast({msg: '下载完毕'});
         }
     });
 }
